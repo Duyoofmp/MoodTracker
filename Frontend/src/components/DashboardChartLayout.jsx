85b8259d9a9e70cffd4a334681ccb9d930f3383a
AswinCG <cgaswin333@gmail.com>
2023-10-28 16:32:58 +0530

    chart ui fix

--- Frontend/src/components/DashboardChartLayout.jsx
+++ Frontend/src/components/DashboardChartLayout.jsx
@@ -5,20 +5,40 @@ export default function DashboardChartLayout({ data }) {
   console.log(data)
   
   return (
-    <AreaChart
-      width={1000}
-      height={500}
-      data={data}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Area type="monotone" dataKey="Sad" stroke="red" fill="red" fillOpacity={0.2} />
-      <Area type="monotone" dataKey="Neutral" stroke="yellow" fill="yellow" fillOpacity={0.2} />
-      <Area type="monotone" dataKey="Happy" stroke="green" fill="green" fillOpacity={0.2} />
-    </AreaChart>
-  );
+		<div className="-mt-10 mr-2">
+			<AreaChart
+				width={1000}
+				height={500}
+				data={data}
+				margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+			>
+				<CartesianGrid strokeDasharray="3 3" />
+				<XAxis dataKey="name" />
+				<YAxis />
+				<Tooltip />
+				<Legend />
+				<Area
+					type="monotone"
+					dataKey="Sad"
+					stroke="red"
+					fill="red"
+					fillOpacity={0.2}
+				/>
+				<Area
+					type="monotone"
+					dataKey="Neutral"
+					stroke="yellow"
+					fill="yellow"
+					fillOpacity={0.2}
+				/>
+				<Area
+					type="monotone"
+					dataKey="Happy"
+					stroke="green"
+					fill="green"
+					fillOpacity={0.2}
+				/>
+			</AreaChart>
+		</div>
+	);
 }
